dc2ffa45df35f3388aff04d85a967b7d666ae2e2
PapaBear01 <Korosky11@gmail.com>
2026-06-01 16:09:33 +0100

    Fix: Clean up vite config

--- vite.config.ts
+++ vite.config.ts
@@ -19,26 +19,12 @@ export default defineConfig({
         scope: "/",
         start_url: "/",
         icons: [
-          {
-            src: "icon-192.png",
-            sizes: "192x192",
-            type: "image/png"
-          },
-          {
-            src: "icon-512.png",
-            sizes: "512x512",
-            type: "image/png"
-          },
-          {
-            src: "icon-512.png",
-            sizes: "512x512",
-            type: "image/png",
-            purpose: "any maskable"
-          }
+          { src: "icon-192.png", sizes: "192x192", type: "image/png" },
+          { src: "icon-512.png", sizes: "512x512", type: "image/png" },
+          { src: "icon-512.png", sizes: "512x512", type: "image/png", purpose: "any maskable" }
         ]
       },
       workbox: {
-        // Disable SW caching during dev to prevent stale bundles
         maximumFileSizeToCacheInBytes: 0,
       },
     })
